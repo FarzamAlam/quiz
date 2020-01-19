f09524d4370ee90f584a0499ae518412ff14626f
FarzamAlam <farzamcse@gmail.com>
2020-01-19 16:14:35 +0530

    Added a timer

--- src/main.go
+++ src/main.go
@@ -6,6 +6,7 @@ import (
 	"fmt"
 	"os"
 	"strings"
+	"time"
 )
 
 type problem struct {
@@ -16,6 +17,7 @@ type problem struct {
 func main() {
 	// create a flag and parse it.
 	csvFileName := flag.String("csv", "problems.csv.txt", "A csv file in the format question, answer.")
+	timer := flag.Int("timer", 5, "Timer in seconds that will stop the quiz if answer doesn't come.")
 	flag.Parse()
 
 	lines, err := readFile(csvFileName)
@@ -24,8 +26,8 @@ func main() {
 	}
 	problems := parseFile(lines)
 
-	finalScore := showQuiz(problems)
-	fmt.Println(finalScore)
+	finalScore := showQuiz(problems, timer)
+	fmt.Printf("\n%s", finalScore)
 }
 
 // readFile takes the name of the file and returns the string slice of all the columns.
@@ -57,14 +59,25 @@ func parseFile(lines [][]string) []problem {
 }
 
 //showQuiz displays the problem one by one and returns the final score.
-func showQuiz(problems []problem) string {
+func showQuiz(problems []problem, timer *int) string {
 	correct := 0
 	for i, p := range problems {
+		timeLimit := time.NewTimer(time.Duration(*timer) * time.Second)
 		fmt.Printf("Problem #%d: %s\t", i+1, p.ques)
-		var answer string
-		fmt.Scanf("%s\n", &answer)
-		if answer == p.ans {
-			correct++
+		answerCh := make(chan string)
+		go func() {
+			var answer string
+			fmt.Scanf("%s\n", &answer)
+			answerCh <- answer
+		}()
+
+		select {
+		case <-timeLimit.C:
+			return fmt.Sprintf("Final Score is %d of %d", correct, len(problems))
+		case answer := <-answerCh:
+			if answer == p.ans {
+				correct++
+			}
 		}
 	}
 	return fmt.Sprintf("Final Score is %d of %d", correct, len(problems))
