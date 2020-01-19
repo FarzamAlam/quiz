da4a81d878270839c265ac01af6cda56295e050f
FarzamAlam <farzamcse@gmail.com>
2020-01-19 15:52:31 +0530

    Refractored main() into multiple func()

--- src/main.go
+++ src/main.go
@@ -18,22 +18,30 @@ func main() {
 	csvFileName := flag.String("csv", "problems.csv.txt", "A csv file in the format question, answer.")
 	flag.Parse()
 
-	file, err := os.Open(*csvFileName)
+	lines, err := readFile(csvFileName)
 	if err != nil {
-		fmt.Printf("Error while opening %s\n", *csvFileName)
-		os.Exit(1)
-	}
-	defer file.Close()
-	r := csv.NewReader(file)
-	lines, err := r.ReadAll()
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "Error while reading the file: %v", err)
-		os.Exit(1)
+		fmt.Fprintf(os.Stderr, "Error while reading the file : %s \n %v", *csvFileName, err)
 	}
 	problems := parseFile(lines)
 
 	finalScore := showQuiz(problems)
 	fmt.Println(finalScore)
+}
+
+// readFile takes the name of the file and returns the string slice of all the columns.
+func readFile(csvFileName *string) ([][]string, error) {
+	file, err := os.Open(*csvFileName)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+	r := csv.NewReader(file)
+	lines, err := r.ReadAll()
+	if err != nil {
+		return nil, err
+
+	}
+	return lines, nil
 }
 
 //parseFile takes the input as [][]string and parses it to array of struct problem.
